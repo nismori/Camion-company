19ddbab4eabdb65b1e43c26cf09db881a329aecb
Yuliscqua <105552280+Yuliscqua@users.noreply.github.com>
2024-02-02 18:45:14 +0100

    Create t.sh

---
+++ t.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#On a bien data.csv en argument
+if [ $# -eq 0 ]; then
+    echo "Le fichier data n'existe pas"
+    exit 1
+fi
+
+#temps actuel en nanosecondes
+start_time=$(date +%s.%N)
+
+#On utilise un AVL dans une fonction c pour trouver les éléments
+cat < $2 | cut -d ';' -f1,f5 > data.txt
+s.c > s.txt
+rm data.txt
+
+#On affiche le résultat avec gnuplot : on configure les paramètres du graphique (description des axes, valeurs des axes, largeur des boîtes, décalages...) puis on créé le graphique pivoté lors de l'affichage
+echo $data.txt
+
+#temps actuel en nanosecondes
+end_time=$(date +%s.%N)
+
+#Calcul de la différence de temps
+execution_time=$(echo "$end_time - $start_time" | bc)
+
+#Affichage du temps d'execution
+echo "Temps d'exécution du script : $execution_time secondes"
